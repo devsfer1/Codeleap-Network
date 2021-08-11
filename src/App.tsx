3c10ff3f7e1fd5b87a7a1c48d97dc24e720db595
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-11 00:47:56 -0300

    fix: useEffect in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import { Flex } from '@chakra-ui/react'
 import { PagesMain } from './pages/Main'
@@ -7,11 +7,25 @@ import { PagesSignUp } from './pages/Signup'
 import { SignUpFormData } from './interfaces/mock'
 import { useDispatch } from 'react-redux'
 import { updateUser } from './redux/userSlice'
+import { updatePosts } from './redux/postsSlice'
+
+import mockServices from './actions/mock'
 
 const App: React.FC = () => {
     const [loggedIn, setLoggedIn] = useState<boolean>(false)
 
     const dispatch = useDispatch()
+
+    const handleGetPosts = useCallback(async () => {
+        const { _getAll } = mockServices()
+
+        const data = await _getAll()
+        dispatch(updatePosts(data.results))
+    }, [dispatch])
+
+    useEffect(() => {
+        handleGetPosts()
+    }, [handleGetPosts])
 
     const handleSignUp = (data: SignUpFormData | undefined) => {
         dispatch(updateUser(data?.name))
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,10 @@
-import React, { useCallback, useEffect } from 'react'
 import { Post } from '../Post'
-import mockServices from '../../actions/mock'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectPosts, updatePosts } from '../../redux/postsSlice'
+import { useSelector } from 'react-redux'
+import { selectPosts } from '../../redux/postsSlice'
 
 export function PagesHomeList(): JSX.Element {
 
     const { posts } = useSelector(selectPosts)
-
-    const dispatch = useDispatch()
-
-    const handleGetPosts = useCallback(async () => {
-
-        const { _getAll } = mockServices()
-
-        const data = await _getAll()
-        dispatch(updatePosts(data.results))
-    }, [dispatch])
-
-    useEffect(() => {
-        handleGetPosts()
-    }, [handleGetPosts])
 
     return (
         <>
